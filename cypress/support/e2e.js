a6eeef701dbe6a373d3ece20d661e669039fb454
katerina-imediate <katexrg@yahoo.com>
2024-12-03 01:09:41 +0200

    install dev dep

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -17,12 +17,4 @@
 //import './commands'
 
 // Alternatively you can use CommonJS syntax:
-// require('./commands')
-beforeEach(() => {
-  // cy.intercept middleware to remove 'if-none-match' headers from all requests
-  // to prevent the server from returning cached responses of API requests
-  cy.intercept(
-    { url: "http://localhost:3001/**", middleware: true },
-    (req) => delete req.headers["if-none-match"]
-  );
-});
+ import'./commands'
