c56251f72c932e9790198dd1c06a51c8360909e7
katerina-imediate <katexrg@yahoo.com>
2024-12-02 20:30:51 +0200

    edit cy conf

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   defaultCommandTimeout: 10000,
 
   e2e: {
+    supportFile: "cypress/support/e2e.js",
     setupNodeEvents(on, config) {
      // supportFile: "cypress/support/e2e.ts"
     },
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,11 @@
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+beforeEach(() => {
+  // cy.intercept middleware to remove 'if-none-match' headers from all requests
+  // to prevent the server from returning cached responses of API requests
+  cy.intercept(
+    { url: "http://localhost:3001/**", middleware: true },
+    (req) => delete req.headers["if-none-match"]
+  );
+});
